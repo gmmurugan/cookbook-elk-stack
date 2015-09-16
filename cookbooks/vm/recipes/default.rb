50d668ca42032af33b984723cfeaa90cbe1124e3
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2015-09-16 14:01:20 +0200

    update apt first otherwise installation fails on a vanilla box

--- cookbooks/vm/recipes/default.rb
+++ cookbooks/vm/recipes/default.rb
@@ -6,3 +6,6 @@
 #
 # Copyright (c) 2015 Zühlke, All Rights Reserved.
 
+#This frst, otherwise installation fails because the apt cache is not updated
+include_recipe "apt"
+include_recipe 'elasticsearch'
