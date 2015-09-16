da3e731be2a7bb886f2ce961353fb2906f4a3fd2
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2015-09-16 13:14:07 +0200

    vm cookbook scaffolding

---
+++ cookbooks/vm/metadata.rb
@@ -0,0 +1,10 @@
+name             'vm'
+maintainer       'var@zuehlke.com'
+maintainer_email 'var@zuehlke.com'
+license          'MIT '
+description      'Elastic Search + Kibana as a test backend'
+long_description 'Elastic Search + Kibana as a test backend'
+version          '0.1.0'
+
+depends 'elasticsearch', "~>1.0.1"
+
---
+++ cookbooks/vm/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: elastic-test
+# Recipe:: default
+#
+# Author: var
+#
+# Copyright (c) 2015 Zühlke, All Rights Reserved.
+
