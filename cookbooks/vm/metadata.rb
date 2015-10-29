9ab91f0c834fbb4a17919c18cf7c3231204177ff
Alexander Zenger <zena@zuehlke.com>
2015-10-29 15:14:29 +0100

    fixed cookbooks dependcy versions like recommended by tkn

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -6,7 +6,7 @@ description      'Elastic Search + Kibana as a test backend'
 long_description 'Elastic Search + Kibana as a test backend'
 version          '0.1.0'
 
-depends 'java', '~> 1.35.0'
-depends 'elasticsearch', "~>1.0.3"
-depends 'kibana_lwrp', '~> 2.0.6'
-depends 'simple-logstash', '~> 0.2.4'
+depends 'java', '1.35.0'
+depends 'elasticsearch', "1.0.3"
+depends 'kibana_lwrp', '2.0.6'
+depends 'simple-logstash', '0.2.4'
